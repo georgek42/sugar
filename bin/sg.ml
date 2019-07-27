d112c4965f55f31962a35762cf6f8a18811c8e03
George Kharchenko <hkharc2@uic.edu>
2019-07-27 14:34:29 -0400

    Add CLI, examples

--- bin/sg.ml
+++ bin/sg.ml
@@ -1,27 +1,39 @@
 open Core
 open Sugar
-open Sugar.Opcode
 open Bitstring
 
-let () = ()
-  (* let prog = [|
-    (* main *)
-    Pushi 3;
-    Pushi 2;
-    Pushi 1;
-    Call "mean";
-    Call "vm_print_int";
-    Ret;
-    (* mean (pc: 6) *)
-    Pop 1;
-    Addi;
-    Addi;
-    Pop 2;
-    Pushi 3;
-    Pushr 2;
-    Divi;
-    Pushr 1;
-    Ret;
-  |] in
-  Vm.addl "mean" 6;
-  Vm.run prog ~debug:false; *)
+let help () =
+  printf "
+Sugar 0.0.1
+Commands:
+  run <asm_file>.sugar -- Compile and execute a sugar assembly file
+  compile <asm_file>.sugar <target_file>.sg -- Compile a sugar assembly file to bytecode
+  exec <bytecode>.sg -- Execute a sugar bytecode file
+
+Flags:
+  --debug -- Enable debug messages
+"
+
+let () =
+  if Array.length Sys.argv < 3 then help () else (
+    match Array.get Sys.argv 1 with
+    | "run" -> (
+      let filename = Array.get Sys.argv 2 in
+      let debug = if Sys.argv |> Array.filter ~f:(fun a -> a = "debug") |> Array.length > 0 then true else false in
+      let prog = Asm.from_file filename |> Compile.compile in
+      Vm.run prog ~debug:debug
+    )
+    | "compile" when Array.length Sys.argv < 4 -> help ()
+    | "compile" -> (
+      let filename = Array.get Sys.argv 2 in
+      let out_filename = Array.get Sys.argv 3 in
+      Asm.from_file filename |> Compile.compile |> Pack.to_file out_filename
+    )
+    | "exec" -> (
+      let filename = Array.get Sys.argv 2 in
+      let debug = if Sys.argv |> Array.filter ~f:(fun a -> a = "debug") |> Array.length > 0 then true else false in
+      let prog = Pack.from_file filename in
+      Vm.run prog ~debug:debug
+    )
+    | c -> printf "Unrecognized command: %s" c; help ();
+  )
--- src/pack.ml
+++ src/pack.ml
@@ -21,12 +21,13 @@ let pack (op: opcode): bitstring =
   | Pushi i -> make_unary 0 i
   | Pushr rid -> make_unary 1 rid
   | Pop i -> make_unary 2 i
-  | Call name -> raise Not_implemented
+  | Syscall id -> make_unary 7 id
+  | Call pc -> make_unary 6 pc
   | Ret -> make 3
   | Addi -> make 4
   | Divi -> make 5
 
-let unpack (b: bitstring): opcode array =
+let unpack (b: bitstring): program =
   let b = ref b in
   let res = Stack.create () in
   let shift_by n = b := subbitstring !b n (bitstring_length !b - n) in
@@ -50,6 +51,16 @@ let unpack (b: bitstring): opcode array =
         | {| op: 8; param: 32 |} -> Pop (Int32.to_int_exn param) |> add; shift_by 40
         | {| _ |} -> raise Not_implemented
       )
+      | 6 -> (
+        match%bitstring !b with
+        | {| op: 8; param: 32 |} -> Call (Int32.to_int_exn param) |> add; shift_by 40
+        | {| _ |} -> raise Not_implemented
+      )
+      | 7 -> (
+        match%bitstring !b with
+        | {| op: 8; param: 32 |} -> Syscall (Int32.to_int_exn param) |> add; shift_by 40
+        | {| _ |} -> raise Not_implemented
+      )
       | 3 -> add Ret; shift_by 8
       | 4 -> add Addi; shift_by 8
       | 5 -> add Divi; shift_by 8
@@ -60,41 +71,45 @@ let unpack (b: bitstring): opcode array =
   Array.rev_inplace res;
   res
 
-let pack_all (prog: opcode list): bitstring =
-  prog |> List.map ~f:pack |> Bitstring.concat
+let pack_all (prog: program): bitstring =
+  prog |> Array.map ~f:pack |> Array.to_list |> Bitstring.concat
 
-let to_file (filename: string) (prog: opcode list) =
+let to_file (filename: string) (prog: program) =
   Bitstring.bitstring_to_file (pack_all prog) filename
 
 let from_file (filename: string) = Bitstring.bitstring_of_file filename |> unpack
 
 let%test "pack_roundtrip" =
-  let prog = [
+  let prog = [|
     Pushi 1;
     Pushr 6;
     Pop 3;
+    Call 4;
+    Syscall 0;
     Ret;
     Addi;
     Divi
-  ] in
+  |] in
   let packed = pack_all prog in
-  let unpacked = packed |> unpack |> Array.to_list in
-  match List.zip prog unpacked with
+  let unpacked = packed |> unpack in
+  match Array.zip prog unpacked with
   | None -> false
-  | Some l -> List.fold l ~init:true ~f:(fun acc (op, op') -> acc && (op = op'))
+  | Some l -> Array.fold l ~init:true ~f:(fun acc (op, op') -> acc && (op = op'))
 
 let %test "pack_file_roundtrip" =
-  let prog = [
+  let prog = [|
     Pushi 1;
     Pushr 6;
     Pop 3;
+    Call 4;
+    Syscall 0;
     Ret;
     Addi;
     Divi
-  ] in
+  |] in
   prog |> to_file "tmp.sg";
-  let unpacked = from_file "tmp.sg" |> Array.to_list in
+  let unpacked = from_file "tmp.sg" in
   Sys.remove "tmp.sg";
-  match List.zip prog unpacked with
+  match Array.zip prog unpacked with
   | None -> false
-  | Some l -> List.fold l ~init:true ~f:(fun acc (op, op') -> acc && (op = op'))
+  | Some l -> Array.fold l ~init:true ~f:(fun acc (op, op') -> acc && (op = op'))
